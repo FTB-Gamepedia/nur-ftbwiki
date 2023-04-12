e91a3f7b9900ac149a8e57e9937ccb06033cc4b1
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-04-11 20:01:36 -0700

    mediawiki-oredict: init

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  mediawiki-oredict = pkgs.callPackage ./pkgs/mediawiki-plugins/oredict { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
