ef94a401ecfe2bc3814ca91e4d2cf0cede6da751
Tomo <68489118+tomodachi94@users.noreply.github.com>
2023-04-14 05:18:00 +0000

    mediawiki-rs: Init

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   ftb-rs = pkgs.callPackage ./pkgs/utilities/ftb-rs { };
+  mediawiki-rs = pkgs.callPackage ./pkgs/software-libraries/mediawiki-rs { };
   mediawiki-tilesheets = pkgs.callPackage ./pkgs/mediawiki-plugins/tilesheets { };
   mediawiki-oredict = pkgs.callPackage ./pkgs/mediawiki-plugins/oredict { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
